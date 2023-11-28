54d341fc671232f1e5447d335ff14a79d4756dd4
washwin <114923029+washwin@users.noreply.github.com>
2023-11-28 07:40:15 +0530

    multikill error fixed

--- generic/movegen.py
+++ generic/movegen.py
@@ -1,3 +1,6 @@
+#Function parameters: current board object, color of the player for which we need to find available kill moves
+#Return value: killcoins-> contains location of coins that have available kill
+#Return datatype: list
 def checkKill(board,color):
     killcoins = []
     if color == 'red':
@@ -76,6 +79,9 @@ def checkKill(board,color):
     killcoins = (list(t))
     return killcoins
 
+#Function parameters: current board object, color of the player for which we need to find all available moves
+#Return value: movecoins-> contains location of coins that are allowed to move
+#Return datatype: list
 def coinsToMove(board, color):
     movecoins = checkKill(board,color)
     if len(movecoins) > 0:
@@ -133,12 +139,17 @@ def coinsToMove(board, color):
         t = set(movecoins)
         movecoins = list(t)
         return movecoins
+    
 
+#finds whether or not a tile exists
 def isTile(loc):
     if loc[0] not in range(1,9) or loc[1] not in range(1,9):
         return False
     return True
 
+#Function parameters: current board object, location of coin that we intend to move
+#Return value: moves->contains loctions where that particular coin can move to
+#Return datatype: list
 def availableCoinMoves(board, loc):
     i = board.getCoinByPos(loc)
     coin = board.coins[i]
@@ -212,3 +223,13 @@ def availableCoinMoves(board, loc):
             if i==26 and isTile((loc[0]-1,loc[1]+1)):
                 moves.append((loc[0]-1,loc[1]+1))
         return moves
+
+#Function parameters: current board object, location of the coin which we want to check for available multikill moves
+#Return value: True/False
+#Return datatype: boolean 
+def checkMultiKill(board, loc):
+    coin = board.coins[board.getCoinByPos(loc)]
+    if loc in checkKill(board, coin.getColor()):
+        return True
+    else:
+        return False
--- main/two_player_game.py
+++ main/two_player_game.py
@@ -93,6 +93,105 @@ def convertback(point):
             print("ERROR")   
     return op
 
+def makeMove(board, color, mc):    
+    initialflag = True
+    finalflag = True
+    while initialflag:
+        initial = input("CHOOSE COIN: ")
+        if initial.capitalize() == 'Q':
+            print("THANKS FOR PLAYING CHECKERS!")
+            print("EXITING CHECKERS")
+            exit()
+        if initial.capitalize() == 'O':
+            options.displayOptions(0)
+            options.optionBar()
+            board.display()
+            continue
+        if len(initial) != 2:
+            print("INVALID INPUT")
+            continue
+        ip = convert(initial)
+        # mc = movegen.coinsToMove(board,color)
+        if ip not in mc:
+            print("INVALID COIN, CHOOSE FROM THE FOLLOWING")
+            for m in mc:
+                print(convertback(m),end=" ")
+            print("")
+        else:
+            initialflag = False       
+    while finalflag:
+        final = input("PLACE COIN TO: ")
+        if final.capitalize() == 'Q':
+            print("THANKS FOR PLAYING CHECKERS!")
+            print("EXITING CHECKERS")
+            exit()
+        if final.capitalize() == 'O':
+            options.displayOptions(0)
+            options.optionBar()
+            board.display()
+            continue
+        if len(final) != 2:
+            print("INVALID INPUT")
+            continue
+        fp = convert(final)
+        acm = movegen.availableCoinMoves(board,ip)
+        if fp not in acm:
+            print("INVALID MOVE, CHOOSE FROM THE FOLLOWING")
+            for m in acm:
+                print("m: ",m)
+                print(convertback(m),end=" ")
+            print("")
+        else:
+            finalflag = False
+    board.moveCoin(ip,fp)
+    if color == "red":
+        if fp[1] == 8:
+            board.coins[board.getCoinByPos(fp)].setKing()
+    else:
+        if fp[1] == 1:
+            board.coins[board.getCoinByPos(fp)].setKing()
+    return ip, fp
+
+def makeMultiKillMove(board, color, cp):
+    options.optionBar()
+    board.display()
+    print("CHANCE FOR MULTIKILL")
+    finalflag = True    
+    while finalflag:
+        final = input("PLACE COIN {} TO: ".format(convertback(cp)))
+        if final.capitalize() == 'Q':
+            print("THANKS FOR PLAYING CHECKERS!")
+            print("EXITING CHECKERS")
+            exit()
+        if final.capitalize() == 'O':
+            options.displayOptions(0)
+            options.optionBar()
+            board.display()
+            continue
+        if len(final) != 2:
+            print("INVALID INPUT")
+            continue
+        fp = convert(final)
+        acm = movegen.availableCoinMoves(board,cp)
+        if fp not in acm:
+            print("INVALID MOVE, CHOOSE FROM THE FOLLOWING")
+            for m in acm:
+                print("m: ",m)
+                print(convertback(m),end=" ")
+            print("")
+        else:
+            finalflag = False
+    board.moveCoin(cp,fp)
+    if color == "red":
+        if fp[1] == 8:
+            board.coins[board.getCoinByPos(fp)].setKing()
+    else:
+        if fp[1] == 1:
+            board.coins[board.getCoinByPos(fp)].setKing()
+    board.eliminate((int((cp[0]+fp[0])/2), int((cp[1]+fp[1])/2)))
+    if movegen.checkMultiKill(board, fp):
+        makeMultiKillMove(board, color, cp)
+
 def gameOver(board):
     player1 = True
     player2 = True
@@ -115,7 +214,7 @@ def gameOver(board):
 
 def testing(board):
     board.eliminate((1,7))
-    board.eliminate((3,7))
+    # board.eliminate((3,7))
     board.eliminate((5,7))
     board.eliminate((7,7))
     board.eliminate((2,8))
@@ -123,7 +222,7 @@ def testing(board):
     board.eliminate((6,8))
     board.eliminate((8,8))
     board.eliminate((2,6))
-    # board.eliminate((4,6))
+    board.eliminate((4,6))
     board.eliminate((6,6))
     board.eliminate((8,6))
     board.eliminate((1,1))
@@ -135,13 +234,13 @@ def testing(board):
     board.eliminate((6,2))
     board.eliminate((8,2))
     board.eliminate((1,3))
-    board.eliminate((3,3))
+    # board.eliminate((3,3))
     # board.eliminate((5,3))
     board.eliminate((7,3))
 
 def game():
     board = util.Board()
-    # testing(board)
+    testing(board)
     turn = 0
     while not gameOver(board):
         options.optionBar()
@@ -152,66 +251,15 @@ def game():
         else:
             print("PLAYER 2's TURN (BLACK)")
             color = "black"
-        initialflag = True
-        finalflag = True
-        while initialflag:
-            initial = input("CHOOSE COIN: ")
-            if initial.capitalize() == 'Q':
-                print("THANKS FOR PLAYING CHECKERS!")
-                print("EXITING CHECKERS")
-                exit()
-            if initial.capitalize() == 'O':
-                options.displayOptions(0)
-                options.optionBar()
-                board.display()
-                continue
-            if len(initial) != 2:
-                print("INVALID INPUT")
-                continue
-            ip = convert(initial)
-            mc = movegen.coinsToMove(board,color)
-            if ip not in mc:
-                print("INVALID COIN, CHOOSE FROM THE FOLLOWING")
-                for m in mc:
-                    print(convertback(m),end=" ")
-                print("")
-            else:
-                initialflag = False       
-        while finalflag:
-            final = input("PLACE COIN TO: ")
-            if final.capitalize() == 'Q':
-                print("THANKS FOR PLAYING CHECKERS!")
-                print("EXITING CHECKERS")
-                exit()
-            if final.capitalize() == 'O':
-                options.displayOptions(0)
-                options.optionBar()
-                board.display()
-                continue
-            if len(final) != 2:
-                print("INVALID INPUT")
-                continue
-            fp = convert(final)
-            mc = movegen.availableCoinMoves(board,ip)
-            if fp not in mc:
-                print("INVALID MOVE, CHOOSE FROM THE FOLLOWING")
-                for m in mc:
-                    print(convertback(m),end=" ")
-                print("")
-            else:
-                finalflag = False
-        board.moveCoin(ip,fp)
+
+        mc = movegen.coinsToMove(board,color)
+        ip, fp = makeMove(board, color, mc)
+
+        if fp[1] == ip[1] - 2 or fp[1] == ip[1] + 2:
+            board.eliminate((int((ip[0]+fp[0])/2), int((ip[1]+fp[1])/2)))
+            if movegen.checkMultiKill(board, fp):
+                makeMultiKillMove(board, color, fp)
         
-        if color == "red":
-            if fp[1] == ip[1] + 2 or fp[1] == ip[1] - 2:
-                board.eliminate((int((ip[0]+fp[0])/2), int((ip[1]+fp[1])/2)))
-            if fp[1] == 8:
-                board.coins[board.getCoinByPos(fp)].setKing()
-        else:
-            if fp[1] == ip[1] - 2 or fp[1] == ip[1] + 2:
-                board.eliminate((int((ip[0]+fp[0])/2), int((ip[1]+fp[1])/2)))
-            if fp[1] == 1:
-                board.coins[board.getCoinByPos(fp)].setKing()
         print("")
         print("")
         print("")
