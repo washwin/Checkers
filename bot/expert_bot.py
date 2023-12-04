b17221d5fe4314678c7eb7699e30a80ef7f1d66f
washwin <114923029+washwin@users.noreply.github.com>
2023-12-04 12:33:48 +0530

    starting expert bot

--- bot/expert_bot.py
+++ bot/expert_bot.py
@@ -9,9 +9,33 @@ from generic import movegen
 from generic import util
 
 #DEFINING PARAMETERS
+MIN = -100
+MAX = 100
 home_coin_score = 2
 border_coin_score = 1
 king_coin_score = 3
 chaining_score = 1
 
-def alphaBeta()
+def alphaBeta(depth, nodeIndex, maximizingPlayer, values, alpha, beta):
+    if depth == 3:
+        return values[nodeIndex]
+    
+    if maximizingPlayer:
+        best = MIN
+        for i in range(0, 2):
+            val = alphaBeta(depth + 1, nodeIndex*2 + i, False, values, alpha, beta)
+            best = max(best, val)
+            alpha = max(alpha, best)
+            if beta <= alpha:
+                break
+        return best
+    
+    else:
+        best = MAX
+        for i in range(0, 2):
+            val = alphaBeta(depth + 1, nodeIndex*2 + i, False, values, alpha, beta)
+            best = max(best, val)
+            beta = max(alpha, best)
+            if beta <= alpha:
+                break
+        return best
